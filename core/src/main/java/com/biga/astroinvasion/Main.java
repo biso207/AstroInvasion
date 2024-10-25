e9217dcb9daac128c9329affa0d9a51f0a8149fa
luca <luca.bisognin13@gmail.com>
2024-10-25 21:49:09 +0200

    init

---
+++ core/src/main/java/com/biga/astroinvasion/Main.java
@@ -0,0 +1,33 @@
+package com.biga.astroinvasion;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture img;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("lobby_marketplace_ita.png");  // LibGDX fornisce questa immagine di esempio
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  // Pulisce lo schermo
+        batch.begin();
+        batch.draw(img, 0, 0);  // Disegna l'immagine a coordinate (0, 0)
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+}
