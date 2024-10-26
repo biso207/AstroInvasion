def24dd31204a1c6b9672b8c438d679c7cbfede5
luca <luca.bisognin13@gmail.com>
2024-10-26 20:31:38 +0200

    Changes by Matteo

--- core/src/main/java/com/biga/astroinvasion/Main.java
+++ core/src/main/java/com/biga/astroinvasion/Main.java
@@ -1,11 +1,13 @@
 package com.biga.astroinvasion;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class Main extends Game {
 
     @Override
     public void create() {
-        this.setScreen(new LoadingScreen(this));
+        Gdx.input.setInputProcessor(new MyInputProcessor()); // Imposta MyInputProcessor per gestire gli input
+        this.setScreen(new LoadingScreen(this)); // Schermata di caricamento
     }
 }
