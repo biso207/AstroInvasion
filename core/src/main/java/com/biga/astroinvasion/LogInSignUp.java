02993f61dede6c7a04079ced03824d05b46b2163
luca <luca.bisognin13@gmail.com>
2024-11-13 22:47:51 +0100

    lavoro 13/11/2024

--- core/src/main/java/com/biga/astroinvasion/LogInSignUp.java
+++ core/src/main/java/com/biga/astroinvasion/LogInSignUp.java
@@ -19,16 +19,14 @@ public class LogInSignUp extends ScreenAdapter {
     private SpriteBatch screen;
     private Texture img1, img2, img3, img4;
     private BitmapFont font;
-    private StringBuilder nicknameInput;
-    private StringBuilder passwordInput;
+    private final StringBuilder nicknameInput;
+    private final StringBuilder passwordInput;
     private boolean enteringNickname;
     private boolean enteringPassword;
     public String nickname;
     private int state = 0; // 0 = LogIn, 1 = errore LogIn, 2 = SignUp, 3 = errore SignUp
-    private final Main game;
 
     public LogInSignUp(Main game) {
-        this.game = game;
         this.screen = game.screen;
         this.enteringNickname = true;
         this.enteringPassword = true;
@@ -36,7 +34,7 @@ public class LogInSignUp extends ScreenAdapter {
         // Carica il font
         loadFont();
 
-        // initi stringhe
+        // init stringhe
         nicknameInput = new StringBuilder();
         passwordInput = new StringBuilder();
 
@@ -51,7 +49,7 @@ public class LogInSignUp extends ScreenAdapter {
         @Override
         public boolean keyTyped(char character) {
             if (enteringNickname) {
-                if (character == '\n' || character == '\r') { // ENTER per terminare il nickname
+                if (character == '\n' || character == '\r' && nicknameInput.length()>=1) { // ENTER per terminare il nickname
                     enteringNickname = false;
                 } else if (character == '\b' && nicknameInput.length() > 0) { // BACKSPACE per cancellare l'ultimo carattere
                     nicknameInput.deleteCharAt(nicknameInput.length() - 1);
@@ -59,13 +57,29 @@ public class LogInSignUp extends ScreenAdapter {
                     nicknameInput.append(character);
                 }
             } else if (enteringPassword) {
-                if (character == '\n' || character == '\r') { // ENTER per terminare la password
+                if (character == '\n' || character == '\r' && passwordInput.length()>=1) { // ENTER per terminare la password
                     enteringPassword = false;
                 } else if (character == '\b' && passwordInput.length() > 0) { // BACKSPACE per cancellare l'ultimo carattere
                     passwordInput.deleteCharAt(passwordInput.length() - 1);
                 } else if (character >= 32 && character < 127 && passwordInput.length()<=20) { // Controllo per caratteri validi
                     passwordInput.append(character);
                 }
+            }
+            return true;
+        }
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            // click pulsante "crea nuovo profilo" da pagina accesso
+            if ((state==0 || state==1) && (screenX >= 288 && screenX <= 479) && (screenY >= 525 && screenY <= 565)) {
+                state=2;
+            }
+            // click pulsante "avanti" da pagina "accesso" o "accesso con errore"
+            if ((state==0 || state==1) && (nicknameInput.length()>=1&&passwordInput.length()>=1) && (screenX >= 520 && screenX <= 710) && (screenY >= 525 && screenY <= 565)) {
+                state=4;
+            }
+            // click pulsante "avanti" da pagina "registrazione" o "registrazione con errore"
+            if ((state==2 || state==3) && (nicknameInput.length()>=1&&passwordInput.length()>=1) && (screenX >= 385 && screenX <= 620) && (screenY >= 525 && screenY <= 565)) {
+                state=4;
             }
             return true;
         }
@@ -100,43 +114,48 @@ public class LogInSignUp extends ScreenAdapter {
     }
 
     public void SignUpAlg() {
-        File generalFolder = new File("data/" + nicknameInput);
-        File dataFolder = new File("data/" + nicknameInput + "/data_user");
+        try {
+            // percorsi nuovo utente
+            FileHandle generalFolder = Gdx.files.local("data/" + nicknameInput);
+            FileHandle dataFolder = Gdx.files.local("data/" + nicknameInput + "/data_user");
 
-        if (!generalFolder.exists()) {
-            generalFolder.mkdir();
-            dataFolder.mkdir();
+            if (!generalFolder.exists() && !dataFolder.exists()) {
+                generalFolder.mkdirs();
+                dataFolder.mkdirs();
 
-            try {
-                FileWriter writeNick = new FileWriter("data/" + nicknameInput + "/data_user/nickname.txt");
-                writeNick.write(String.valueOf(nicknameInput));
-                writeNick.close();
+                // scrittura del nickname digitato
+                FileHandle writeNick = Gdx.files.local("data/" + nicknameInput + "/data_user/nickname.txt");
+                writeNick.writeString(String.valueOf(nicknameInput), false);  // `false` sovrascrive il file se già esiste
 
-                FileWriter writePass = new FileWriter("data/" + nicknameInput + "/data_user/password.txt");
-                writePass.write(String.valueOf(passwordInput));
-                writePass.close();
+                // scrittura della password digitata
+                FileHandle writePass = Gdx.files.local("data/" + nicknameInput + "/data_user/password.txt");
+                writePass.writeString(String.valueOf(passwordInput), false);  // `false` sovrascrive il file se già esiste
 
                 nickname = String.valueOf(nicknameInput);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+
+                state=4;
             }
-        } else {
-            state = 3;
+            else if ((generalFolder.exists() && dataFolder.exists()) && (nicknameInput.length()>=1 || passwordInput.length()>=1)) {
+                state=3;
+            }
+        }
+        catch (Exception ignored) {
         }
     }
 
     public void LogInAlg() {
         try {
-            File readPass = new File("data/" + nicknameInput + "/data_user/password.txt");
-            String readPasswordInput = new Scanner(readPass).nextLine();
+            // lettura password nickname e password dai file
+            FileHandle readPass = Gdx.files.internal("data/" + nicknameInput + "/data_user/password.txt");
+            FileHandle readNick = Gdx.files.internal("data/" + nicknameInput + "/data_user/nickname.txt");
+            String filePassword = readPass.readString();
+            String fileNickname = readNick.readString();
 
-            if (!readPasswordInput.equals(String.valueOf(passwordInput))) {
+            if (!filePassword.equals(String.valueOf(passwordInput)) || !fileNickname.equals(String.valueOf(nicknameInput))) {
                 state = 1;
-            }
-        } catch (IOException e) {
-            state = 1;
+            } else state = 4;
+        }catch(Exception ignored){
         }
-
         nickname = String.valueOf(nicknameInput);
     }
 
@@ -147,7 +166,7 @@ public class LogInSignUp extends ScreenAdapter {
         } catch (Exception e) {
             Gdx.app.log("Font Error", "Il font non è stato caricato correttamente: " + e.getMessage());
             font = new BitmapFont();
-            font.setColor(Color.valueOf("#151A3B")); // Blu nostro
+            font.setColor(Color.valueOf("#151A3B")); // Imposta il colore anche nel font di default
         }
     }
 
@@ -156,6 +175,7 @@ public class LogInSignUp extends ScreenAdapter {
         img2 = new Texture("login_signup_pages/page_1_log_in_eng_error.png");
         img3 = new Texture("login_signup_pages/page_2_sign_up_eng.png");
         img4 = new Texture("login_signup_pages/page_2_sign_up_eng_error.png");
+        img5 = new Texture("lobby_marketplace_ita.png");
 
         enteringNickname = true;
         Gdx.input.setInputProcessor(new MyInputProcessor()); // Riattiva l'InputProcessor
@@ -180,6 +200,9 @@ public class LogInSignUp extends ScreenAdapter {
                 break;
             case 3:
                 screen.draw(img4, 0, 0);
+                break;
+            case 4:
+                screen.draw(img5, 0, 0);
                 break;
             default:
                 break;
